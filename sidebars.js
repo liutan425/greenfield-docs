19ac64b5c44b5b63e8dd57c78f575984a344472e
Alexxxxxx <118710506+alexgao001@users.noreply.github.com>
2023-10-12 21:45:06 +0800

    docs: Relayer (#233)
    * docs: relayer
    
    * docs: add DB host explanation
    
    * docs: fix relayer doc title

--- sidebars.js
+++ sidebars.js
@@ -94,7 +94,8 @@ const sidebars = {
             "guide/greenfield-blockchain/run-node/run-local-network",
             "guide/greenfield-blockchain/run-node/run-mainnet-node",
             "guide/greenfield-blockchain/run-node/run-testnet-node",
-            "guide/greenfield-blockchain/run-node/run-testnet-relayer-and-challenger",
+            "guide/greenfield-blockchain/run-node/run-relayer",
+            "guide/greenfield-blockchain/run-node/run-challenger",
             "guide/greenfield-blockchain/run-node/become-testnet-validator",
           ]
         }
